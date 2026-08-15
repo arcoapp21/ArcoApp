7bfc7743ad37f4ca625a3fe7e2d9e174342f7fbc
arcoapp21 <arcoapp.21@gmail.com>
2026-08-15 13:24:06 -0400

    Update config-uss.js

--- config-uss.js
+++ config-uss.js
@@ -4,5 +4,4 @@
  * Reemplaza la URL por la de tu Worker una vez desplegado. Todos los módulos
  * la leen desde aquí: no hay que tocar ningún HTML.
  */
-window.FICHA_USS_API = "https://ficha-uss-proxy.TU-SUBDOMINIO.workers.dev";
- 
+window.FICHA_USS_API = "https://ficha-uss-proxy.arcoapp-21.workers.dev";
